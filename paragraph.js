4b20131dc4ee615fdd882baf24e5e1e5f9cfc927
kbashar <khyrulbashar.0.1@gmail.com>
2016-06-28 17:06:30 +0600

    Paragraph function defination to class definition

--- paragraph.js
+++ paragraph.js
@@ -1,35 +1,32 @@
-function Paragraph(paragraph_text) {
-            var wordArray = paragraph_text.split(" ");
-            var currentWordIndex = 0;
-            var prevTotalCharCount = 0;
-            var totalCharCountArray = [];
-            wordArray.forEach(function(str, index, array) {
-                    var previousCount = (index === 0) ? 0 : totalCharCountArray[index-1];
-                    var spaceCount = (index === (array.length-1)) ? 0: 1;
-                    totalCharCountArray.push(previousCount + str.length + spaceCount);
-                });
-            
-            this.getTotalCharCountAtIndex = function(index) {
-                console.log("requested for: " + index);
-                return index < 0 ? 0 : totalCharCountArray[index];
-            }
-            this.getArray = function() {
-                return wordArray;
-            }
-            this.getCurrentIndex = function() {
-                return currentWordIndex;
-            }
-            this.getText = function() {
-                return paragraph_text;
-            }
-            this.getCurrentWord = function() {
-                return wordArray[currentWordIndex];
-            } 
-            this.hasWords = function() {
-                return wordArray.length - 1 > currentWordIndex
-            }
-            this.updateCurrentWordIndex = function() {
-                currentWordIndex = this.hasWords() ? currentWordIndex+1 : -1
-                
-            }
-        };
+class Paragraph {
+    constructor(paragraph_text) {
+         let wordArray = paragraph_text.split(" ");
+         let currentWordIndex = 0;
+         let prevTotalCharCount = 0;
+         let totalCharCountArray = [];
+        wordArray.forEach(function (str, index, array) {
+             let previousCount = (index === 0) ? 0 : totalCharCountArray[index - 1];
+             let spaceCount = (index === (array.length - 1)) ? 0 : 1;
+            totalCharCountArray.push(previousCount + str.length + spaceCount);
+        });
+    }
+    getTotalCharCountAtIndex(index) {
+        console.log("requested for: " + index);
+        return index < 0 ? 0 : totalCharCountArray[index];
+    }
+    getArray() {
+        return wordArray;
+    }
+    getCurrentIndex() {
+        return currentWordIndex;
+    }
+    getCurrentWord() {
+        return wordArray[currentWordIndex];
+    }
+    hasWords() {
+        return wordArray.length - 1 > currentWordIndex
+    }
+    updateCurrentWordIndex() {
+        currentWordIndex = this.hasWords() ? currentWordIndex + 1 : -1
+    }
+}
