3ecba716cf79103f424277bd7a27f492d011c12e
kbashar <khyrulbashar.0.1@gmail.com>
2016-06-28 12:04:07 +0600

    bundler

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: "./app.js",
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: ['/\.js$/', /\.es6$/],
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['','.js','es6']
+  }
+}
