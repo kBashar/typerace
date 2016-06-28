1c13153e3613e19a16ab667e41060acb952455f6
kbashar <khyrulbashar.0.1@gmail.com>
2016-06-29 00:28:04 +0600

    implement basic structure. style the words according to state i.e. done, current, regular.

--- components/textcontainer.js
+++ components/textcontainer.js
@@ -1,13 +1,46 @@
 import React from "react";
 
-export default class TextContainer {
+export default class TextContainer extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  render(){
-    return (
+  spanify() {
+    var regularStyle = {
+      color: "black"
+    }
+    var doneStyle = {
+      color: "green",
+    }
+    var currentStyle = {
+      color: "white",
+      backgroundColor: "green"
+    }
+    let currentWordIndex = this.props.currentWordIndex;
 
+    return this.props.wordArray.map(function (word, index) {
+      let style;
+      if (currentWordIndex === index) {
+        style = currentStyle;
+      }
+      else if (index < currentWordIndex) {
+        style = doneStyle;
+      }
+      else {
+        style = regularStyle;
+      }
+      return (<span
+        style = {style}
+        key={index}>{word}
+      </span>)
+    })
+  }
+
+  render() {
+    return (
+      <div id="wordContainer">
+        {this.spanify()}
+      </div>
     );
   }
 }
