b480025d3fa33c69dee9ee4fe69373b5dcbac1f2
kbashar <khyrulbashar.0.1@gmail.com>
2016-06-28 16:18:55 +0600

    Chnages

---
+++ app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TypeRace from "./components/typerace"
+
+//ReactDOM.render(<ScoreBoard score="33"/>, document.getElementById("root"));
+ReactDOM.render(<TypeRace />, document.getElementById("root"));
+
