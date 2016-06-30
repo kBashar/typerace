2dfafa98fa48bc044dbb92af2a6b04f69109a47c
kbashar <khyrulbashar.0.1@gmail.com>
2016-06-30 17:14:10 +0600

    clean app.js

--- app.js
+++ app.js
@@ -9,16 +9,3 @@ import LandingPage from "./landingpage";
 //var paragraph = new Paragraph("The difference is scoping. var is scoped to me");
 //ReactDOM.render(<TypeRace paragraph = {paragraph} />, document.getElementById("root"));
 ReactDOM.render(<LandingPage />, document.getElementById("root"));
-
-u1 = {
-    type : "race",
-    id: "race1",
-    body:{
-        doc: {
-            pid1:{
-                wpm: 12,
-                accuracy: 89
-            }
-        }
-    }
-}
